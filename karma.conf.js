8fc05175778f69366cd65153f160d716725bb1cd
André <aa.concha7@gmail.com>
2022-03-11 16:02:27 -0300

    ajuste nome do projeto para o build

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/quick-buy.ui'),
+      dir: require('path').join(__dirname, './coverage/quickbuyui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
